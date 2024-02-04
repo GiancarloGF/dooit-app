8e78107f77fedf94ac9bfd2dfb962d949ec89f29
Giancarlo Guerra <gian.guerra23@gmail.com>
2024-02-04 10:04:23 -0500

    App folder in src folder

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -26,8 +26,8 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    Comfortaa: require("../assets/fonts/Comfortaa-Regular.ttf"),
+    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
+    Comfortaa: require("../../assets/fonts/Comfortaa-Regular.ttf"),
     ...FontAwesome.font,
   });
 
